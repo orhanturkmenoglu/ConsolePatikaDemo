1231c1f033570a995a637841104d2dbb80459088
orhan <orhantrkmn749@gmail.com>
2022-01-27 18:03:47 +0300

    type conversion eklendi.

---
+++ Operators/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Operatörler.
+
+            // Atama ve işlemli atama
+
+            int x = 10;
+            int y = 20;
+
+            y = y + 2;  // atama ve artırma işlemi
+            x += 2;     // veya bu şekilde de yapabiliriz işlemli atama
+
+            y /= 3;     // bölü atama işlemi
+
+            x *= 2;  // x = x*2 karşılıgıdır
+
+
+            // Mantıksal operatörler 
+
+
+            // || veya  && ve ! değil
+
+            bool isSuccess = true;
+
+            bool isCompleted = false;
+
+            if (isSuccess && isCompleted)
+            {
+                Console.WriteLine("Perfect");
+            }
+
+            if (isSuccess || isCompleted)
+                Console.WriteLine("Great");
+
+            if (isSuccess && !isCompleted)
+                Console.WriteLine("Fine !");
+
+
+            // ilişkisel operatorler 
+            // <  >  >= <= == !=
+
+            int a = 3;
+            int b = 4;
+            bool sonuc = a < b;
+            Console.WriteLine(sonuc);
+
+            sonuc = a > b;
+            Console.WriteLine(sonuc);
+
+            sonuc = a >= b;
+            Console.WriteLine(sonuc);
+
+            sonuc = a <= b;
+            Console.WriteLine(sonuc);
+
+            sonuc = a != b;
+            Console.WriteLine(sonuc);
+
+            sonuc = a == b;
+            Console.WriteLine(sonuc);
+
+            // Aritmetik operatörler 
+
+            // +, -, /, *, %
+
+            int sayi1 = 10;
+            int sayi2 = 5;
+
+            int sonuc1 = sayi1 /sayi2;
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = sayi1 * sayi2;
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = sayi1 - sayi2;
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = sayi1 + sayi2;
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = sayi1 % sayi2;
+            Console.WriteLine(sonuc1);
+
+
+
+        }
+    }
+}
---
+++ TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace TypeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Type Conversion : Tip dönüşümleri
+
+            /*
+             * Bazı zamanlarda veriler arası tür dönüşümüne ihtiyac duydugumuz zaman kullanmamız gereken yapılardır
+             */
+
+            /*
+             * 
+             *  2 Tip dönüşüm mevcut 
+             *  1-Implicit conversion : Belirgin olmayan automatic gercekleşen program tarafından yapılır
+             *    nedir bu : küçük veri tiplerinin büyük veri tiplernin içine atanmasıdır
+             *  
+             *  2-Explicit conversion : Belirgin olan farklı türler arasında gerçekleşir büyük veri tiplerinin küçük veri tiplerine dönüşmesidir
+             *   veri kaybı yaşanır
+
+             */
+
+            // 1-ımplicit conversion demo
+
+
+            int myInt = 10;
+            long myLong = myInt;
+
+            // 2- Explicit demo
+
+            double myDouble = 15.5;
+            float myFloat = (float)myDouble;
+
+            // 2- Explicit demo
+            string number = "12";
+            int myNumber = int.Parse(number);
+
+
+            // 2- Explicit demo
+            int sinif = 12;
+            string mySinif = sinif.ToString();
+
+
+            // 2- Explicit demo
+            char myChar = 'C';
+            string convertChar = myChar.ToString();
+
+
+
+            byte myByte = 10;
+            int myIntt = myByte;
+
+
+            ParseMethod();
+
+
+
+
+        }
+
+        public static void ParseMethod()
+        {
+            string metin1 = "10";
+            string metin2 = "10.25";
+            int rakam;
+            double double1;
+
+
+            rakam = Int32.Parse(metin1);
+            double1 = Double.Parse(metin2);
+
+            Console.WriteLine("Rakam : "+rakam);
+            Console.WriteLine("Rakam :"+double1);
+
+        }
+    }
+}
